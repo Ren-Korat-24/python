53cc244092d06e0191f0e5b1f26c05ac7661804e
Ren Korat <koratren12@gmail.com>
2025-07-14 19:36:28 +0530

    New Funvtion

---
+++ Dictionaries/Basic.py
@@ -0,0 +1,15 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+
+
+# print(type(thisdict))
+
+# print(len(thisdict))
+
+# print(thisdict["brand"])
+
+# print(thisdict)
---
+++ Set/Basic.py
@@ -0,0 +1,53 @@
+myset={10,20,30,"Hello"}
+myset2={True,False,2233,"Hell0"}
+mtset3={60,70,80}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+
+mtset3^=myset
+print(mtset3)
+
+# mtset3=myset.symmetric_difference(myset2)
+# print(mtset3)
+
+# myset2.pop()
+# print(myset2)
+
+# myset3=myset.issuperset(myset2)
+# print(myset3)
+
+# myset3=myset.issubset(myset2)
+# print(myset3)
+
+# myset3=myset.isdisjoint(myset2)
+# print(myset3)
+
+# myset.discard(10)
+# print(myset)
+
+# mtset3=myset.difference_update(myset2)
+# print(mtset3)
+
+# mtset3=myset.difference(myset2,mtset3,set3,set4)
+# print(mtset3)
+
+# mtset3=myset|myset2
+
+# mtset3=myset.union(myset2,set3,set4)
+# print(mtset3)
+
+# myset.remove(10)
+# print(myset)
+
+# myset.update(myset2)
+# print(myset)
+
+# myset.add("Ai")
+# print(myset)
+
+# for x in myset:
+    # print("hell"in myset)
+    # print(myset)
+    
+# print(type(myset))
+# print(len(myset))
