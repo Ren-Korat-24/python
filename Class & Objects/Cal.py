1b6816b56600c54e4ab3d5b1b6b14b78ac246fa9
Ren Korat <koratren12@gmail.com>
2025-07-17 18:22:55 +0530

    new updates

--- Class & Objects/Cal.py
+++ Class & Objects/Cal.py
@@ -1,22 +1,24 @@
 class Cal:
-    def add(self,a,b):
-        print("Additon:",a+b)
-        
-    def sub(self):
-        print("Substraction:",a-b)
-        
-    def Mul(self):
-        print("\Multiplication:",a*b)
-        
-    def Div(self):
-        print("Divison:",a/b)
-        
-    def Mod(self):
-        print("Modulo:",a%b)
-c=Cal()
+    def add(self, a, b):
+        print("Additon:", a + b)
 
-a=int(input("Enter the A:"))
-b=int(input("Enter the B:"))
+    def sub(self):
+        print("Substraction:", a - b)
+
+    def Mul(self):
+        print("\Multiplication:", a * b)
+
+    def Div(self):
+        print("Divison:", a / b)
+
+    def Mod(self):
+        print("Modulo:", a % b)
+
+
+c = Cal()
+
+a = int(input("Enter the A:"))
+b = int(input("Enter the B:"))
 
 print("\n========== Bank Management System ==========")
 print("Add = 1")
@@ -25,20 +27,21 @@ print("Mul = 3")
 print("Div = 4")
 print("Module = 5")
 print("==========================================")
-choice=int(input("Enter the no:"))
+choice = int(input("Enter the no:"))
 
-if choice==1:
+if choice == 1:
     c.add()
 
-elif choice==2:
+elif choice == 2:
     c.sub()
 
 
-elif choice==3:
+elif choice == 3:
     c.Mul()
 
-elif choice==4:
+elif choice == 4:
     c.Div()
 
-elif choice==5:
+elif choice == 5:
     c.Mod()
+
