846bc9a2750b3a4ccf72965beea36906b60e7b5b
Ren Korat <koratren12@gmail.com>
2025-07-22 12:06:41 +0530

    Projects

---
+++ Projects/Student_Result using class & obejcts.py
@@ -0,0 +1,79 @@
+class Student:
+    #Constructor
+    def __init__(self):
+        self.rno = 0
+        self.name = ""
+        self.hindi = 0
+        self.gujarati = 0  
+        self.ss = 0
+        self.total = 0
+        self.percentage = 0.0
+        self.grade = ""
+        self.min_marks = 0
+        self.max_marks = 0
+
+    #Input from user
+    def inputDetails(self):
+        self.rno = int(input("Enter Roll Number: "))
+        self.name = input("Enter Student Name: ")
+        self.hindi = int(input("Enter marks in Hindi: "))
+        self.gujarati = int(input("Enter marks in Gujarati: "))
+        self.ss = int(input("Enter marks in Social Science (SS): "))
+
+    #Calculate the Marks
+    def calculateResult(self):
+        marks = [self.hindi, self.gujarati, self.ss]
+        self.min_marks = min(marks)
+        self.max_marks = max(marks)
+        self.total = sum(marks)
+        self.percentage = self.total / len(marks)
+
+        if min(marks) >= 33:
+            self.grade = "Pass"
+        else:
+            self.grade = "Fail"
+
+    #Display the result
+    def displayResult(self):
+        print("\nResult Summary:\n")
+        print("RollNo\tName\tHindi\tGujarati\tSS\tMin\tMax\tTotal\tPercentage\tGrade")
+        print(f"{self.rno}\t{self.name}\t{self.hindi}\t{self.gujarati}\t\t{self.ss}\t{self.min_marks}\t{self.max_marks}\t{self.total}\t{self.percentage:.2f}%\t\t{self.grade}")
+
+    #File Handling
+    def studentHistory(self):
+        with open("Student_Records.txt", "a") as s:
+            s.write("\t\t\t\t\t\tStudent Records ")
+            s.write(f"\nRoll No\t\tName\tHindi\tGujarati\tSS\t\tTotal\tPercentage\tGrade")
+
+        with open("Student_Records.txt", "a") as s:
+            s.write(f"\n{self.rno}\t\t\t{self.name}\t\t{self.hindi}\t\t{self.gujarati}\t\t\t{self.ss}\t\t{self.total}\t\t{self.percentage:.2f}%\t\t{self.grade}\n")
+
+# Object
+s = Student()
+
+# Main Program
+while True:
+    print("\n========== Student Result System ==========")
+    print("1: Student Details")
+    print("2: Calculate Result")
+    print("3: Display Result")
+    print("4: Exit")
+    print("==========================================")
+    choice = int(input("Choice: "))
+
+    match choice:
+        case 1:
+            s.inputDetails()
+
+        case 2:
+            s.calculateResult()
+            
+        case 3:
+            s.displayResult()
+            s.studentHistory()
+
+        case 4:
+            print("Thank you for using the Student Result System. Goodbye!")
+            break
+        case _:
+            print("Invalid Choice. Please try again...")
---
+++ Projects/bank.py
@@ -0,0 +1,169 @@
+import os
+
+class Bank:
+    #Constructor
+    def __init__(self):
+        self.name = ""
+        self.acnum = 0
+        self.password = ""
+        self.balance = 0
+
+    #Basic Details
+    def personDetails(self):
+        self.name = input("Enter the name: ")
+        # check Passoword
+        while True:
+            self.password = input("Enter the Password (min 8 characters): ")
+            if len(self.password) >= 8:
+                break
+            else:
+                print("Password is too short...")
+    #Check Balance
+        while True:
+            self.balance = int(input("Enter Amount (minimum 2000): "))
+            if self.balance >= 2000:
+                break
+            else:
+                print("Minimum deposit is 2000!")
+
+    #Display
+    def displayDetails(self):
+        print(f"{self.name}\t{self.acnum}\t\t{self.balance}")
+
+    #Credit
+    def creditMoney(self):
+        money = int(input("Enter the Amount to deposit: "))
+        self.balance += money
+        print("Credit Successful.Updated balance:", self.balance)
+
+    #Debit
+    def withdrawMoney(self):
+        money = int(input("Enter the Amount to withdraw: "))
+        if money > self.balance:
+            print("Insufficient Balance...")
+        else:
+            self.balance -= money
+            print("Withdrawal Successful...\nRemaining Balance:", self.balance)
+
+    #Tranfer
+    def transferMoney(self, amount, receiver):
+        if amount > self.balance:
+            print("Insufficient Balance.Transfer Failde..")
+        else:
+            self.balance -= amount
+            receiver.balance += amount
+            print(f"₹{amount} transferred successfully to Account {receiver.acnum}")
+            print(f"Your remaining balance: ₹{self.balance}")
+
+#File Handling
+def bank_records():
+    a =open("bankRecords.txt","a")
+    # if not os.path.exists("bankRecords.txt") or os.path.getsize("bankRecords.txt") == 0:
+    with open("bankRecords.txt", "a") as a:
+            a.write(f"\nName\tAccount No\tBalance\t\tPassword")
+
+    with open("bankRecords.txt", "a") as a:
+        a.write(f"\n{b.name}\t\t{b.acnum}\t\t\t{b.balance}\t\t{b.password}")
+
+#Store
+AcountDetails = []
+
+#account function
+def get_data(acnum):
+    for a in AcountDetails:
+        if a.acnum == acnum:
+            return a
+    return None
+
+
+while True:
+    print("\n========== Bank Management System ==========")
+    print("\n1: Open Account")
+    print("2: Show Accounts")
+    print("3: Credit")
+    print("4: Withdrawal")
+    print("5: Tranfer")
+    print("6: Exit")
+    print("==========================================")
+    choice = int(input("Choice: "))
+
+    match choice:
+        case 1:
+            n = int(input("Number of new accounts to open: "))
+            for _ in range(n):
+                while True:
+                    acnum = int(input("Enter a unique account number: "))
+                    if get_data(acnum):
+                        print("Account already exists! Try another number.")
+                    else:
+                        break
+                b = Bank()
+                b.acnum = acnum  # Set unique account number
+                b.personDetails()
+                AcountDetails.append(b)
+                print("Account created successfully.")
+                bank_records()
+
+        case 2:
+            print("\nName\tAccount no\tBalance")
+            for a in AcountDetails:
+                a.displayDetails()
+
+        case 3:
+            fnum = int(input("Enter account number: "))
+            account = []
+            for a in AcountDetails:
+                if a.acnum == fnum:
+                    account = a
+                    break
+            if account:
+                repass = input("Enter password: ")
+                if account.password == repass:
+                    a.creditMoney()
+                else:
+                    print("Invalid password...")
+            else:
+                print("Account not found.Go to the case 1....")
+            bank_records()
+
+        case 4:
+            fnum = int(input("Enter account number: "))
+            account = None
+            for a in AcountDetails:
+                if a.acnum == fnum:
+                    account = a
+                    break
+            if account:
+                repass = input("Enter password: ")
+                if account.password == repass:
+                    a.withdrawMoney()
+                else:
+                    print("Incorrect password.")
+            else:
+                print("Account not found.Go to the case 1....")
+            bank_records()
+
+        case 5:
+            fnum1 = int(input("Enter account number of sender: "))
+            fnum2 = int(input("Enter account number of receiver: "))
+
+            sender = get_data(fnum1)
+            receiver = get_data(fnum2)
+
+            if sender and receiver:
+                repass = input("Enter the Password:")
+                if sender.password == repass:
+                    amount = int(input("Enter amount to transfer: "))
+                    sender.transferMoney(amount, receiver)
+                else:
+                    print("Invalid Password...")
+            else:
+                print("Invalid sender or receiver account number...")
+            bank_records()
+
+        case 6:
+            print("Thank you for using the Bank System. Goodbye!")
+            break
+
+        case _:
+            print("Invalid choice. Please try again.")
